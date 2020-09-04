8f94c28d3154807ead4c0af9a47d0b27132fe7ed
tavo <tavo_x99@hotmail.com>
2020-09-04 16:39:52 -0500

    proyecto base

--- apps/menu/models.py
+++ apps/menu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+# class Menu(models.Model):
+#     nombre = models.CharField(max_length=100)
+#     precio = models.DecimalField(max_digits=6, decimal_places=2)
+#     descripcion = models.TextField(max_length=300)
+#
+#     class Meta:
+#         db_table = 'menu'
+#
+#     def __str__(self):
+#         return '{} - ${} - {}'.format(self.nombre, self.precio, self.descripcion)
--- apps/menu/views.py
+++ apps/menu/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def create(request):
+    pass
+
+
+def read_one(request):
+    pass
+
+
+def read_all(request):
+    pass
+
+
+def update(request):
+    pass
+
+
+def delete(request):
+    pass
